c6df5a0740cedc25c353b165702835b6abda92cb
Cherro <kaliszewski.dominik@gmail.com>
2018-06-15 14:13:39 +0200

    Remove _poi model from map service

--- niehandlowa/src/app/services/map.service.ts
+++ niehandlowa/src/app/services/map.service.ts
@@ -6,6 +6,6 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class MapService {
-  //private _poi: POI = new POI;
+
   constructor(private _http: HttpClient) { }
 }
