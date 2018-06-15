e878594c7defe22f8a7bb632010e1556f69d4cf4
Cherro <kaliszewski.dominik@gmail.com>
2018-06-15 17:07:15 +0200

    Initialization template-driven forms in addPOI Component

--- niehandlowa/src/app/components/add-poi/add-poi.component.ts
+++ niehandlowa/src/app/components/add-poi/add-poi.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-import {DateAdapter} from '@angular/material';
+import POI from '../../models/POI';
+
 
 @Component({
   selector: 'app-add-poi',
@@ -8,16 +9,19 @@ import {DateAdapter} from '@angular/material';
   styleUrls: ['./add-poi.component.css']
 })
 export class AddPOIComponent implements OnInit {
-  public openDate: Date;
-  time = {hour: 13, minute: 30};
-  constructor(private _toastr: ToastrService,
-    private _dateAdapter: DateAdapter<Date>) {
-    this._dateAdapter.setLocale('pl');
+  public newPOI: POI = new POI;
+
+  constructor(private _toastr: ToastrService) {
+
    }
 
   ngOnInit() {
     // setTimeout(() => this._toastr.success('Kurłaaa','Janusz Notification!'));
-    this.time = {hour: 13, minute: 30};
+
+  }
+
+  public onSubmit() {
+    console.log(this.newPOI);
   }
 
 }
