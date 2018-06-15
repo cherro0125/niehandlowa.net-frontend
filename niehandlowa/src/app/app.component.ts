19834fe3190a59ecd86cde262eb238d19f117464
Cherro <kaliszewski.dominik@gmail.com>
2018-06-15 11:21:51 +0200

    Repair gitignore

--- niehandlowa/src/app/app.component.ts
+++ niehandlowa/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  images: Array<string>;
+
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit() {
+  }
+
 }
