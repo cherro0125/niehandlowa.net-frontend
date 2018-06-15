a44ed9278c6471a65b364ec845b147a23f8eebf8
toyerovsky <przemyslaw.postrach@hotmail.com>
2018-06-15 12:07:05 +0200

    Add folder structures

--- niehandlowa/src/app/app.module.ts
+++ niehandlowa/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
