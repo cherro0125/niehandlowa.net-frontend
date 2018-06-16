e64fd220dd609731c854c573eb38204b289f7f28
Cherro <kaliszewski.dominik@gmail.com>
2018-06-16 10:47:08 +0200

    Add type in poi list

--- niehandlowa/src/app/views/poi-list/poi-list.component.ts
+++ niehandlowa/src/app/views/poi-list/poi-list.component.ts
@@ -23,7 +23,7 @@ export class PoiListComponent implements OnInit {
     public _search: string;
     public _show: boolean;
 
-    displayedColumns = ['name', 'address', 'description', 'other'];
+    displayedColumns = ['name', 'address', 'description', 'type' , 'other'];
 
     constructor(
         public dialog: MatDialog,
