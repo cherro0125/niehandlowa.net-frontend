54b2fa24853cf3b1ff9f4511986e04baac262569
toyerovsky <przemyslaw.postrach@hotmail.com>
2018-06-16 08:30:39 +0200

    Fix closeDialog()

--- niehandlowa/src/app/views/dialogs/more-poi-dialog/more-poi-dialog.component.ts
+++ niehandlowa/src/app/views/dialogs/more-poi-dialog/more-poi-dialog.component.ts
@@ -16,22 +16,9 @@ export class MorePoiDialogComponent implements OnInit {
   ) {
   }
 
-  // private iterator: number = 1;
-
-  // private _days: string[] =
-  //   [{ number: 1, name: 'Poniedziałek' },
-  //   { number: 2, name: 'Wtorek' },
-  //   { number: 3, name: 'Środa' },
-  //   { number: 4, name: 'Czwartek' },
-  //   { number: 5, name: 'Piątek' },
-  //   { number: 6, name: 'Sobota' },
-  //   { number: 7, name: 'Sobota' },
-  //   { number: 8, name: 'Niedziela' },
-  //   { numberd: 9, name: 'Niedziela (niehandlowa)' }];
-
   ngOnInit() {
     this.poi = this.data.poi;
-    for (let i = 0 ; i < 8; i++) {
+    for (let i = 0; i < 8; i++) {
       if ((this.poi.openingHours[i] !== undefined) && (this.poi.openingHours[i].dayOfWeek !== i + 1)) {
         const iter = this.poi.openingHours[i].dayOfWeek - 1;
         const tmp_o = this.poi.openingHours[i];
@@ -47,13 +34,11 @@ export class MorePoiDialogComponent implements OnInit {
         this.poi.openingHours[i] = { dayOfWeek: j, openingTime: undefined, closingTime: undefined };
         j++;
 
-    //   } else {
-    //     j = this.poi.openingHours[i].dayOfWeek + 1;
-    //   }
+      } else {
+        j = this.poi.openingHours[i].dayOfWeek + 1;
+      }
 
     }
-
-
   }
   closeDialog() {
     this.dialogRef.close();
